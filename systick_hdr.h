ed0263cbf0f2a8f22ca274989022bd345eb62837
ahmedmohmedels <ae@users.noreply.github.com>
2021-01-18 19:25:48 +0200

    redefined datatype | unsigned long >> uint_32

--- systick_hdr.h
+++ systick_hdr.h
@@ -1,7 +1,15 @@
-#define SysTick_base		(unsigned long)0xE000E010
-#define	SysTick_CTRL		(*((volatile unsigned long*)(SysTick_base + 0x00)))
-#define	SysTick_LOAD		(*((volatile unsigned long*)(SysTick_base + 0x04)))
-#define	SysTick_VAL			(*((volatile unsigned long*)(SysTick_base + 0x08)))
+/*
+** SysTick_Driver 
+** Eng: Ahmed Mohamed Elsmahy
+** This Driver Written for STM32F103C8 Microcontroller
+** For more info: whatsapp/ +201091822055
+*/
+
+#define uint_32					 unsigned long
+#define SysTick_base		(uint_32)0xE000E010
+#define	SysTick_CTRL		(*((volatile uint_32*)(SysTick_base + 0x00)))
+#define	SysTick_LOAD		(*((volatile uint_32*)(SysTick_base + 0x04)))
+#define	SysTick_VAL			(*((volatile uint_32*)(SysTick_base + 0x08)))
 
 //speed
 #define FCLK			1
@@ -12,9 +20,9 @@
 #define INT			1
 
 
-void SYSTICK_INIT(unsigned long initval,unsigned short speed, unsigned short interrupt);
+void SYSTICK_INIT(uint_32 initval,unsigned short speed, unsigned short interrupt);
 void SYSTICK_START();
 void SYSTICK_STOP();
-unsigned long SYSTICK_VAL();
+uint_32 SYSTICK_VAL();
 
 void SYSTICK_DELAYms(unsigned short n);
